1f0310fb3581d35b080f2964a2bafc5419889588
ClauRodriguez <claudio.rodriguez@intive.com>
2025-04-27 14:12:36 -0300

    Added comments, fixed messages into the menus

--- utils.py
+++ utils.py
@@ -1,20 +1,20 @@
 import time
 import random
 
-def get_num_entero(mensaje):
+def get_num_entero(mensaje): #Devuelve un numero entero ingresado por teclado
     num = input(f"{mensaje}")
     while not es_numero_entero(num):
         num = input(f"ERROR... {mensaje}")
     return int(num)
 
-def es_numero_entero(num):
+def es_numero_entero(num): #Valida que el numero por parametro sea un numero entero
     try:
         int(num) 
         return True 
     except ValueError:
         return False
     
-def decimal_to_binario(num):
+def decimal_to_binario(num): #Convierte un numero decimal a binario
     binario = ""
     while num > 0:
         resto = num % 2
@@ -22,17 +22,17 @@ def decimal_to_binario(num):
         binario = str(resto) + binario
     return int(binario)
 
-def binario_to_decimal(num):
+def binario_to_decimal(num): #Convierte un numero binario a decimal
     decimal = 0
     binario = str(num)
     for i in range(len(binario)):
         decimal += int(binario[-(i + 1)]) * (2 ** i)
     return decimal
 
-def get_numero_aleatorio(num1, num2):
+def get_numero_aleatorio(num1, num2): #Gnerera un numero aleatorio entre 2 numeros pasados por parametro
     return random.randint(num1, num2)
 
-def adivinar_numero(num):
+def adivinar_numero(num): #Verifica que el numero ingresado por parametro coincida con el generado aleatoriamente
     userNum = get_num_entero("Ingrese un numero: ")
     intentos = 1
     while userNum != num:
@@ -41,7 +41,10 @@ def adivinar_numero(num):
     message = f"Numero ADIVINADO!!!\nIntentos necesarios: {intentos}"
     return message
 
-def bienvenida():
+def add_simbolo(simbolo, cant): #Agrega un simbolo x cantidad de veces
+    print(f"\n {simbolo * cant}")
+
+def bienvenida(): #Menu de bienvenida
     print("\n(っ◔ o ◔)っ  Bienvenidos al trabajo integrador \n")
     print("     El siguente programa muestra dos opciones diferentes de juego")
     print("Abajo se extendera un menu de opciones para elegir....\n")
@@ -52,12 +55,12 @@ def menuPrincipal():
     print("|    Opcion 2: Juego de Adivinan el numero           |\n")
     print("|    Opcion 3: Cerrar programa                       |\n")
 
-def menu1():
+def menu1(): #Menu del juego Conversión de Números
     print("\n|    Opcion 1: Convertir numero DECIMAL a BINARIO    |\n")
     print("|    Opcion 2: Convertir numero BINARIO a DECIMAL    |\n")
     print("|    Opcion 3: Volver al menu principal              |")
 
-def menu2():
+def menu2(): #Menu del juego Adivinanza
     print("\n|    Opcion 1: Adivinar binario              |\n")
     print("|    Opcion 2: Adivinar decimal                |\n")
     print("|    Opcion 3: Volver al menu principal        |\n")
@@ -79,24 +82,27 @@ def select_game():
 
 def game_Conversion_Numeros():
     print("\nSelecciono Proyecto Conversión de Números!!\n")
-    menu1()
     while True:
-        opcion = get_num_entero("\nElija una opción del menú: ")
+        menu1()
+        opcion = get_num_entero("\nElija una opción (1/2/3): ")
         # Procesamos la eleccion del usuario
         if opcion == 1:
             print("\n       Opcion 1 -> Convertir numero DECIMAL a BINARIO\n")
-            num = get_num_entero("Ingrese un numero decimal: ") #pedimos y validamos que el numero ingresado sea un numero entero
-            binario = decimal_to_binario(num) # convertimos el numero decimal a binario mediante la funcion "decimal_to_binario"
+            num = get_num_entero("Ingrese un numero decimal: ") 
+            binario = decimal_to_binario(num)
             print(f"        El numero binario del decimal {num} es: {binario}")
+            add_simbolo("-", 70)
         elif opcion == 2:
             print("\n       Opcion 2 -> Convertir numero BINARIO a DECIMAL\n")
             num = get_num_entero("Ingrese un numero binario: ")
             decimal = binario_to_decimal(num)
             print(f"        El numero decimal del binario {num} es: {decimal}")
-        elif opcion == 3:
-            print("Volviendo al menu principal...\n")
+            add_simbolo("-", 70)
+        elif opcion == 3: # Vuelve al menu principal
+            print("         Volviendo al menu principal...\n")
+            add_simbolo("-", 70)
             menuPrincipal()
-            break  # Vuelve al menu principal
+            break  
         else:
             print("Opción inválida. Intenta de nuevo.")
 
@@ -108,11 +114,12 @@ def game_adivinar_numero():
     num2 = get_num_entero("\nElija un numero: ")
     print("Perfecto! Ahora elijamos que queremos adivinar!")
     print(f"Recorda que el rango del numero aleatorio va a estar entre {num1} y {num2}")
-    menu2()
     while True:
-        opcion = get_num_entero("\nElija una opcion (1/2): ")
+        menu2()
+        opcion = get_num_entero("\nElija una opción (1/2/3): ")
+        # Procesamos la eleccion del usuario
         if opcion == 1:
-            print("\nLa opcion seleccionada es ADIVINAR EL BINARIO...")
+            print("\n       La opcion seleccionada es ADIVINAR EL BINARIO...")
             print("\nEn este juego el programa mostrara un numero en binario y debera adivinar que numero en decimal es :D ")
             num_aleatorio_decimal = get_numero_aleatorio(num1, num2)
             num_aletorio_binario = decimal_to_binario(num_aleatorio_decimal)
@@ -120,9 +127,10 @@ def game_adivinar_numero():
             print("\nQue numero en decimal es? ...")
             mensaje = adivinar_numero(num_aleatorio_decimal)
             print(mensaje)
+            add_simbolo("-", 70)
 
         elif opcion == 2:
-            print("\nLa opcion seleccionada es ADIVINAR EL DECIMAL...")
+            print("\n          La opcion seleccionada es ADIVINAR EL DECIMAL...")
             print("\nEn este juego el programa mostrara un numero en decimal y debera adivinar que numero en binario es :D ")
             num_aleatorio_decimal = get_numero_aleatorio(num1, num2)
             num_binario = decimal_to_binario(num_aleatorio_decimal)
@@ -130,11 +138,12 @@ def game_adivinar_numero():
             print("\nQue numero en binario es? ...")
             mensaje = adivinar_numero(num_binario)
             print(mensaje)
+            add_simbolo("-", 70)
 
-        elif opcion == 3:
-            print("Volviendo al menu principal...\n")
-            menuPrincipal()
-            
+        elif opcion == 3: # Vuelve al menu principal
+            print("     Volviendo al menu principal...\n")
+            add_simbolo("-", 70)
+            menuPrincipal() 
             break
         else:
             print("Opcion incorrecta!! Intente de nuevo")
